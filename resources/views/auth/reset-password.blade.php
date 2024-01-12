b9aef23170f2e9f3a1de093c9b71cfe429221da8
Raymaekers Luca <raymaekers.luca@gmail.com>
2024-01-12 18:02:35 +0100

    Add navigation elements where forgotten and fixes

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,15 @@
 <x-guest-layout>
+    <x-slot name="navigation">
+        <li><a href="/">Home</a></li>
+    </x-slot>
+    <x-slot name="header">
+        <hgroup>
+            <h2 align="center">
+                {{ __('Reset Password') }}
+            </h2>
+        <p align="center"><strong>{{ $request->email }}</strong></p>
+        </hgroup>
+    </x-slot>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -6,9 +17,6 @@
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
         <!-- Email Address -->
-        <x-input-label for="email" :value="__('Email')" />
-        <x-text-input id="email" type="email" name="email" :value="old('email', $request->email)" required autofocus
-            autocomplete="username" />
         <x-input-error :messages="$errors->get('email')" />
 
         <!-- Password -->
